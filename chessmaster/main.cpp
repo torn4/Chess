ac2c48302306df237ebae157b38218690280f701
Simon Jonsson <simon.jonsson@hotmail.com>
2018-09-06 16:10:05 +0200

    printing whiteking ontop of chessboard

--- chessmaster/main.cpp
+++ chessmaster/main.cpp
@@ -5,21 +5,38 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(675, 675), "Playing board");// Rutan som brädet ska ligga i
 
-          sf::Image image;
-        if (!(image.loadFromFile("../Figures/schack.png")))
-                std::cout << "Cannot load image";   //Load Image
+          sf::Image board;
+        if (!(board.loadFromFile("../Figures/schack.png")))
+                std::cout << "Cannot load board image";   //Load Image
 
-        sf::Texture texture;
-        texture.loadFromImage(image);  //Load Texture from image
+         sf::Image whiteKnight;
+        if (!(whiteKnight.loadFromFile("../Figures/whiteKing.png")))
+                std::cout << "Cannot load whiteKing image"; //Load Image
 
-        sf::Sprite sprite;
-        sprite.setTexture(texture);
+        sf::Texture textureBoard;   //Load Texture from image
+        sf::Texture textureWhiteKnight;   //Load Texture from image
+        textureBoard.loadFromImage(board);
+        textureWhiteKnight.loadFromImage(whiteKnight);  //Det är bara den sista bilden som printas, vi vill lägga den sista över den andra
 
+
+        sf::Sprite spriteBoard;
+        sf::Sprite spriteWhiteKnight;
+        spriteBoard.setTexture(textureBoard);
+        spriteWhiteKnight.setTexture(textureWhiteKnight);
+
+        int i=0;
+int a =0;
     while (window.isOpen())
     {
+        i=i+1;
+        if(i==8){
+            i=0;
+            a=a+1;
+            if(a==8)
+                a=0;
+    }
      sf::Event event;
-
-
+     spriteWhiteKnight.setPosition(100*i,100*a);
 
                 while (window.pollEvent(event))
                 {
@@ -28,9 +45,9 @@ int main()
 
                         }//Event handling done
 
-
         window.clear();
-        window.draw(sprite);
+        window.draw(spriteBoard);
+       window.draw(spriteWhiteKnight);
         window.display();
     }
 
