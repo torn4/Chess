d952d266e55b74becfa7958376dec03bbba155d5
Simon Jonsson <simon.jonsson@hotmail.com>
2018-09-06 14:54:47 +0200

    printing a empty chess board

--- chessmaster/main.cpp
+++ chessmaster/main.cpp
@@ -1,22 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(675, 675), "Playing board");// Rutan som brädet ska ligga i
+
+          sf::Image image;
+        if (!(image.loadFromFile("../Figures/schack.png")))
+                std::cout << "Cannot load image";   //Load Image
+
+        sf::Texture texture;
+        texture.loadFromImage(image);  //Load Texture from image
+
+        sf::Sprite sprite;
+        sprite.setTexture(texture);
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+     sf::Event event;
+
+
+
+                while (window.pollEvent(event))
+                {
+                        if (event.type == sf::Event::Closed)
+                                window.close();
+
+                        }//Event handling done
+
 
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 
