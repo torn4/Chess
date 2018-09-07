867ece5b3828c93564eda8eb61d5aa92770e13a7
Simon Jonsson <simon.jonsson@hotmail.com>
2018-09-07 13:09:50 +0200

    Tog bort extra class filer

--- chessmaster/WhiteKing.h
+++
@@ -1,9 +0,0 @@
-
-
-class whiteKing{
-public:
-sf::Sprite wksetvar();
- sf::Sprite wksetPos(int x, int y, sf::Sprite &spriteWhiteKnight)const;
-void wkdraw(sf::RenderWindow &window,const  sf::Sprite &spriteWhiteKnight,bool color);
-
-};
