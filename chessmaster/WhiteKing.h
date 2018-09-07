cefee7469fe4794ab03a5f5d2054dc52d8d298c4
Simon Jonsson <simon.jonsson@hotmail.com>
2018-09-07 09:51:43 +0200

    whikte king, header class

---
+++ chessmaster/WhiteKing.h
@@ -0,0 +1,9 @@
+
+
+class whiteKnight{
+public:
+sf::Sprite wksetvar();
+ sf::Sprite wksetPos(int x, int y, sf::Sprite &spriteWhiteKnight)const;
+void wkdraw(sf::RenderWindow &window,const  sf::Sprite &spriteWhiteKnight);
+
+};
--- chessmaster/main.cpp
+++ chessmaster/main.cpp
@@ -1,42 +1,31 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "WhiteKing.h"
+
+
+
 
 int main()
 {
+     sf::Sprite whiteKnightn;
     sf::RenderWindow window(sf::VideoMode(675, 675), "Playing board");// Rutan som brädet ska ligga i
-
+//
           sf::Image board;
         if (!(board.loadFromFile("../Figures/schack.png")))
                 std::cout << "Cannot load board image";   //Load Image
 
-         sf::Image whiteKnight;
-        if (!(whiteKnight.loadFromFile("../Figures/whiteKing.png")))
-                std::cout << "Cannot load whiteKing image"; //Load Image
-
         sf::Texture textureBoard;   //Load Texture from image
-        sf::Texture textureWhiteKnight;   //Load Texture from image
         textureBoard.loadFromImage(board);
-        textureWhiteKnight.loadFromImage(whiteKnight);  //Det är bara den sista bilden som printas, vi vill lägga den sista över den andra
-
-
         sf::Sprite spriteBoard;
-        sf::Sprite spriteWhiteKnight;
         spriteBoard.setTexture(textureBoard);
-        spriteWhiteKnight.setTexture(textureWhiteKnight);
 
-        int i=0;
-int a =0;
+whiteKnight wk;
+whiteKnightn=wk.wksetvar();
     while (window.isOpen())
     {
-        i=i+1;
-        if(i==8){
-            i=0;
-            a=a+1;
-            if(a==8)
-                a=0;
-    }
+
      sf::Event event;
-     spriteWhiteKnight.setPosition(100*i,100*a);
+
 
                 while (window.pollEvent(event))
                 {
@@ -47,7 +36,9 @@ int a =0;
 
         window.clear();
         window.draw(spriteBoard);
-       window.draw(spriteWhiteKnight);
+        whiteKnightn =  wk.wksetPos(5,5,whiteKnightn);
+        wk.wkdraw(window,whiteKnightn );
+      //
         window.display();
     }
 
